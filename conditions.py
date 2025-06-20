9cfa99758095ca71ee2f90ef1db8c6d14345123d
dilli1492001 <dilliprasad149@gmail.com>
2025-06-20 11:15:23 +0530

    assignment-7

---
+++ conditions.py
@@ -0,0 +1,44 @@
+# temp = int(input("Enter your temperature : "))
+# specify = input("enter what do you want celsius or fahrenheit : ")
+# if specify == 'fahrenheit':
+#     print((temp*9/5)+32,"Fahrenheit")
+# else :
+#     print(temp,"Celsius")
+
+
+
+# corret_username = "dilli"
+# correct_password = "1234"
+# username = input("enter your username : ")
+# password = input("enter your password : ")
+# if username == corret_username:
+#     print("wait")
+#     if password == correct_password:
+#         print("Login successfully!")
+#     else :
+#         print("Incorrect password")
+# else :
+#     print("user not fonnd")
+
+
+
+
+# secret_number = 9
+# number = int(input("enter your number : "))
+# if number == secret_number:
+#     print("congrats ! your guess is correct ")
+# elif number < secret_number:
+#     print("guess tooo low")
+# else :
+#     print("too high")
+
+
+
+total_amount = int(input("enter your  total amount : " ))
+if total_amount > 100:
+    print(f"your total amount is {total_amount*0.1}/-")
+elif total_amount>50:
+    print(f"your total amont is {total_amount*0.05}")
+else :
+    print(f"your total amount is {total_amount}/-")
+
