135dadef1274b29d4062e7a38074933c775e7fad
dilli1492001 <dilliprasad149@gmail.com>
2025-06-26 21:27:08 +0530

    assignment - 10

--- quiz.py
+++ quiz.py
@@ -1,22 +1,65 @@
-question =[
-    {
-        "question":"what is the capital of india ?",
-        "answer":["A.Ap","B.TG","C.DELHI","D.NONE"],
-        "answer":"C"
-    },
-    {
-        "question":"1+2 = ?",
-        "answer":["A.1","B.3","C.2","D.6"],
-        "answer":"B"
-    }
-
+paper =[
+    {"question":"2+2=?",
+     "options":["A.1","B.5","C.4"],
+     "answer":"C"},
+     {
+         "question":"2/2 = ?",
+         "options":["A.1","B.2","C.3"],
+         "answer":"A"
+     },
+     {
+         "question":"2*2 = ?",
+         "options":["A.4","B.2","C.1"],
+         "answer":"A"
+     }
 ]
-count = 0
-for i in question:
-    print(f"Question : ",i["question"])
-    print(f"question : ",i["answer"])
-    a = input("enter your answer[A,B,C,D] : ")
-    if a == i["answer"]:
-        print("correct answer : a")
-        count +=1
-print(f"your score : {count}/2")
+score = 0
+attemp = []
+for i , q in enumerate(paper):
+    print(f"Question {i+1} : {q.get("question")}")
+    for options in q["options"]:
+        print(options)
+    chooice  = input("Enter your question : ")
+    attemp_entry = (i, chooice , q["answer"])
+    if chooice.upper() == q["answer"]:
+        score += 2
+    else :
+        score -= 0.5
+    attemp.append(attemp_entry)
+    print(" ")
+
+
+print(f"YOUR SCORE {score}")
+
+print(f"SUMMARY")
+print(f"******* user answer ******** correct answer")
+for entry in attemp:
+    index,user_answer,correct_answer = entry
+    print(f"Question {index+1} :     {user_answer} :         {correct_answer}")
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
